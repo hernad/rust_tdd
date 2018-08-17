09de1f25174ca7d03d6dbcdec5cc557ce33c9da9
Ernad Husremovic <hernad@bring.out.ba>
2018-08-17 12:41:23 +0200

    test 21

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ fn euler_problem_1_test_1() {
     assert_eq!(euler_problem_1(10), 23 );
 }
 
+#[test]
+fn euler_problem_1_test_2() {
+    assert_eq!(euler_problem_1(21), 98 );
+}
+
 fn euler_problem_1( _max: u64) -> u64 {
 
  (0.._max).map( |x| {println!("{}", x) ; x} ).filter(|n| n % 3 == 0 || n % 5 == 0).sum()
