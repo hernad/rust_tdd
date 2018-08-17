e9b83237cb31ff445a8ead8ae24844b997e032e9
Ernad Husremovic <hernad@bring.out.ba>
2018-08-17 12:15:05 +0200

    spreadsheet example

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,6 @@ fn euler_problem_1_test_1() {
 
 fn euler_problem_1( _max: u64) -> u64 {
 
-  let mut result = 0;
-  for i in 0.._max {
-    if i % 3 == 0 || i % 5 == 0 {
-      result += i;
-    }
-  }
-  result
+ (0.._max).filter(|n| n % 3 == 0 || n % 5 == 0).sum()
 
 }
