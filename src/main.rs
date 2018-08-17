e209bd40c52613dd670f3a4be3fca517f60c9b95
Ernad Husremovic <hernad@bring.out.ba>
2018-08-17 11:18:29 +0200

    prva implementacija - neuspjesno

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,19 @@
 fn euler_problem_1_test_1() {
     assert_eq!(euler_problem_1(10), 23 );
 }
+
+fn euler_problem_1( _max: u64) -> u64 {
+  
+  let mut result = 1;
+  let mut i = 0;
+  loop {
+    if i >= _max {
+      break;
+    }
+    if i % 3 == 0 || i % 5 == 0 {
+      result += i;
+    }
+    i += 1;
+  }
+  result
+}
