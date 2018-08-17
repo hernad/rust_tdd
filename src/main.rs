39ccd4e6703dcaf4dcd48e0b724473fb462b3cb7
Ernad Husremovic <hernad@bring.out.ba>
2018-08-17 11:35:48 +0200

    jedna for petlja

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,13 @@ fn euler_problem_1_test_1() {
 }
 
 fn euler_problem_1( _max: u64) -> u64 {
-  
-  let mut result = 1;
-  let mut i = 0;
-  loop {
-    if i >= _max {
-      break;
-    }
+
+  let mut result = 0;
+  for i in 0.._max {
     if i % 3 == 0 || i % 5 == 0 {
       result += i;
     }
-    i += 1;
   }
   result
+
 }
